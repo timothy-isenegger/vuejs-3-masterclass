732619e9f62d0a39586b80c8ee8af2c86eb9df5f
Timothy <timmy.isenegger@gmail.com>
2021-06-22 14:23:30 +0200

    [TASK] Finish lesson 14

--- src/main.js
+++ src/main.js
@@ -1,20 +1,6 @@
 import {createApp} from 'vue';
 import App from './App.vue';
-import PageHome from "./components/PageHome";
-import { createRouter, createWebHistory } from 'vue-router';
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: PageHome
-  }
-]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
+import router from '@/router';
 
 const forumApp = createApp(App);
 forumApp.use(router);
