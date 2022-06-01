ef14aaf16a8a19dc66cedad592e4a4d8e9a379aa
Timothy <timmy.isenegger@gmail.com>
2022-06-01 17:18:09 +0200

    [TASK] Create component PostEditor, update ThreadShow

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="col-full">
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <textarea v-model="postText" name="" id="" cols="30" rows="10" class="form-input"></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostEditor",
+  data() {
+    return {
+      postText: '',
+    }
+  },
+  methods: {
+    save() {
+      const postId = '9999' + Math.random();
+      const post = {
+        id: postId,
+        text: this.postText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        userId: 'FsCDAk9w8NeXEceLV87arpsXjnQ2'
+      }
+
+      this.$emit('save-post', { post });
+
+      this.postText = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
